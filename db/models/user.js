b6f8d7ed6b0b7ded91403c1ce43442fdf51b8e83
JorgeR-Hi <jrjorgemessi@gmail.com>
2023-06-27 15:32:13 -0500

    created intial users

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,29 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
 
-module.exports = {
-  // add your database adapter fns here
-  getAllUsers,
-};
+// add your database adapter fns here
+
+async function createUser({username, password, email}){
+
+  try{
+    const {rows:[user]}= await client.query(`
+    INSERT INTO users(username, password, email)
+    VALUES ($1, $2, $3)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING id, username, email
+    `, [username, password, email])
+
+    return user
+  }catch(err){
+    throw err
+  }
+}
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
 }
+
+module.exports = {
+createUser,
+getAllUsers,
+};
